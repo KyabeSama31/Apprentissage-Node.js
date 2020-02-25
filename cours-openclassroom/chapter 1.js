825b095d9627831294d3e9548a5927f0ff9905a5
Guigui <guillaume.ros@epsi.fr>
2020-02-25 10:20:28 +0100

    récupération des paramètres d'un url

--- cours-openclassroom/chapter 1.js
+++ cours-openclassroom/chapter 1.js
@@ -1,5 +1,25 @@
 var http = require('http');
 var url = require('url');
+var querystring = require('querystring');
+
+var server = http.createServer(function(req, res) {
+    var params = querystring.parse(url.parse(req.url).query);
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    if ('prenom' in params && 'nom' in params) {
+        res.write('Vous vous appelez ' + params['prenom'] + ' ' + params['nom']);
+    }
+    else {
+        res.write('Vous devez bien avoir un prénom et un nom, non ?');
+    }
+    res.end();
+});
+server.listen(8080);
+
+
+
+
+/*var http = require('http');
+var url = require('url');
 
 var server = http.createServer(function(req, res) {
     var page = url.parse(req.url).pathname;
@@ -19,27 +39,7 @@ var server = http.createServer(function(req, res) {
     }
     res.end();
 });
-server.listen(8080);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+server.listen(8080);*/
 
 
 
